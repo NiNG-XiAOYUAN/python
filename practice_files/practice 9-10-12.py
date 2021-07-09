2bd5d992f257b37218c7d9878dec142274618c60
NiNG-XiAOYUAN <xiaoyuan.ning@foxmail.com>
2021-07-09 13:15:22 +0800

    change

--- practice_files/practice 9-10-12.py
+++ practice_files/practice 9-10-12.py
@@ -5,7 +5,7 @@ from admin import User,Admin
  #from moudle_name import * 导入所有（数量一共就两个）
 #9-12
 from car_2 import Car
-import car_2_1
+from car_2_1 import Battery,Electriccar_3
 '''
 Restaurant_1 = Restaurant("china","www")
 Restaurant_1.get_icecream()
@@ -13,14 +13,15 @@ Restaurant_1.get_icecream()
 admin_1 = Admin("arwges","werhghw",23)
 admin_1.privileges.show_privileges()
 '''
-my_ecar_2 = car_2_1.Electriccar_3('changan','cs75',2019)
-print(my_ecar_2.battery.get_range())
+my_ecar_2 = Electriccar_3('changan','cs75',2019)
+my_ecar_2.battery.get_range()
+my_ecar_2.battery.describe_battery()
 print(my_ecar_2.get_descriptive_name())
-print(my_ecar_2.fill_gas_tank())
+my_ecar_2.fill_gas_tank()
 
-my_ecar_3 = Car('changan','cs85',2020)
-print(my_ecar_3.get_descriptive_name())
-print(my_ecar_3.fill_gas_tank())
+my_car_3 = Car('changan','cs85',2020)
+print(my_car_3.get_descriptive_name())
+my_car_3.fill_gas_tank()
 
 
 
