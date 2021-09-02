e153ac499020e4bdf8fb28c8dc1458fe6da8c896
NiNG-XiAOYUAN <xiaoyuan.ning@foxmail.com>
2021-09-02 18:13:07 +0800

    jj

--- note_files/file_reader.py
+++ note_files/file_reader.py
@@ -1,3 +1,6 @@
-with open('pi_digits.txt') as file_object:
-    contents = file_object.read()
-print(contents)
+filename = 'pi_digits.txt'
+with open(filename) as file_object:
+    # contents = file_object.read()
+    for line in file_object:
+        print(line.rstrip())
+#print(contents.rstrip())
--- practice_files/圆的面积.py
+++ practice_files/圆的面积.py
@@ -1,5 +1,5 @@
 import math 
-r = float(input("vsfvf:"))
+r = float(input("main:"))
 x = math.pi  * r *r
 
 print("圆的面积=",round(x,2))
